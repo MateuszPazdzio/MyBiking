14a91a4a786765218bfd3f24ee24e90fc3e264df
Mateusz Paździo <matipazdzio@gmail.com>
2024-03-12 11:43:20 +0100

    Adding ajax query for RideTimeActivity

--- src/MyBiking.MVC/wwwroot/js/RideActivity/RideActivityMonth.js
+++ src/MyBiking.MVC/wwwroot/js/RideActivity/RideActivityMonth.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
 
     $(".dataRow").hide();
-    $(".monthWrapper").click(function () {
+    $(".monthWrapper").click(function (event) {
         $(this).parent().parent().find(".dataRow").slideToggle()
 
-        if ($(this).children("span").hasClass("text-danger")) {
+        if (!$(this).children("span").hasClass("text-danger")) {
 
             $(this).children("span").removeClass("text-danger").addClass("text-secondary")
             //call db for ride details
+            let year = $('#Year').val;
+            //wiersz tytu³owy
+            let titleRowHtmlElement = event.target.parentElement.parentElement
+            //wiersz z danymi zaagregowanymi
+            let aggregatedDatarowHtmlElement = titleRowHtmlElement.nextSibling
+            
+            let month = titleRowHtmlElement.firstElementChild.textContent.trim()
+            var response = getRideDetails(month, aggregatedDatarowHtmlElement);
         }
         else {
 
@@ -17,3 +25,29 @@ $(document).ready(function () {
 
     });
 });
+
+
+function getRideDetails(month, aggregatedDatarowHtmlElement) {
+
+
+    return $.ajax({
+        url: "/RideController/Details",
+        type: "post",
+        data: {
+            "Year": $("#Year").val(),
+            "Month": month,
+        },
+        success: function (data) {
+            console.log(data)
+            console.log("succses")
+            if (!data.results.length) {
+                aggregatedDatarowHtmlElement.html("No repositories found")
+            } else {
+                fillCodeReposWrapper(data, codeReposWrapper)
+            }
+        },
+        error: function () {
+            console.log("fail")
+        }
+    })
+}
