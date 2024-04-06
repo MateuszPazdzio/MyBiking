2ca358b3b6b567c2fab888d2446d9b1b4b9cbf66
Mateusz Paździo <matipazdzio@gmail.com>
2024-04-06 19:54:11 +0200

    Adding css styles for create and preview button

---
+++ src/MyBiking.MVC/wwwroot/js/frontPage.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    console.log("xd")
+    $("#createBtn").animate({
+        opacity: 1,
+    }, 500, function () {
+    });
+
+    $("#previewBtn").animate({
+        opacity: 1,
+    }, 500, function () {
+        // Animation complete.
+    });
+
+});
