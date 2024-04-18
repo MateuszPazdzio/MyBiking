655130742e01d197a4ceb7d2f5642cd24254387a
Mateusz Paździo <matipazdzio@gmail.com>
2024-04-18 17:35:38 +0200

    Adding wheeliemaxv calculation, that ignore null wheelieRides

--- src/MyBiking.Application/Functions/Query/Ride/MonthlyAgregatedRideResponse.cs
+++ src/MyBiking.Application/Functions/Query/Ride/MonthlyAgregatedRideResponse.cs
@@ -4,9 +4,9 @@
     {
         public double Distance {  get; set; }
         public int Rides {  get; set; }
-        public double WheelieMaxV {  get; set; }
+        public double? WheelieMaxV {  get; set; }
         public double TotalWheelieDistance{  get; set; }
-        public int Wheelies {  get; set; }
+        public int? Wheelies {  get; set; }
         
     }
 }
